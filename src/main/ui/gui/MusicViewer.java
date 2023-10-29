669d6325a9e583716325e16a3cf91ebbad1591ee
linda <llee20@students.cs.ubc.ca>
2023-10-29 16:33:09 -0700

    adjusted bg img and added popup option pane

--- src/main/ui/gui/MusicViewer.java
+++ src/main/ui/gui/MusicViewer.java
@@ -4,36 +4,51 @@ import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.WindowEvent;
 
 public class MusicViewer {
+    JFrame frame;
+
     public static void main(String[] args) {
-        JFrame f = new JFrame(); //creating instance of JFrame
+        new MusicViewer();
+    }
+
+    public MusicViewer() {
+        frame = new JFrame(); //creating instance of JFrame
 
         final JTextField tf = new JTextField();
         tf.setBounds(300,250, 150,25);
-
-        ImageIcon rawImgIcon = new ImageIcon("src/main/ui/gui/swirl.png");
-        Image resizedImg = rawImgIcon.getImage().getScaledInstance(400, 300, Image.SCALE_FAST);
-        ImageIcon imgIcon = new ImageIcon(resizedImg);
-        JLabel label = new JLabel(imgIcon);
-        label.setBounds(175, 150, 400, 300); // You can use your own values
-        f.add(label); //f.getContentPane().add(label);
+        tf.setText("Welcome to MusicViewer");
 
         JButton b = new JButton("Open MusicViewer"); //creating instance of JButton
         b.setBounds(300, 300, 150, 30); //x-axis, y-axis, width, height
 
         b.addActionListener(new ActionListener() {
             public void actionPerformed(ActionEvent e) {
-                tf.setText("Welcome to MusicViewer");
+                tf.setText("Make a playlist");
+                String playlistName = JOptionPane.showInputDialog(frame,"Enter playlist name");
             }
         });
 
-        f.add(b); //adding button in JFrame
-        f.add(tf);
+        frame.add(b); //adding button in JFrame
+        frame.add(tf);
+        setUpJFrame(frame);
+    }
+
+    private void setUpJFrame(JFrame f) {
+        f.add(makeBgImage());
         f.setSize(800,800); //400 width and 500 height
         f.setLayout(null); //using no layout managers
         f.setVisible(true); //making the frame visible
         f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    }
 
+    private JLabel makeBgImage() {
+        ImageIcon rawImgIcon = new ImageIcon("src/main/ui/gui/abstract.png");
+        Image resizedImg = rawImgIcon.getImage().getScaledInstance(900, 900, Image.SCALE_FAST);
+        ImageIcon imgIcon = new ImageIcon(resizedImg);
+        JLabel label = new JLabel(imgIcon);
+        label.setBounds(0, 0, 900, 900); // You can use your own values
+        return label;
     }
 }
