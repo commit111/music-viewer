c2bc0ae91cca99d312ac49cbdd747e37733174c7
linda <llee20@students.cs.ubc.ca>
2023-11-04 12:05:31 -0700

    better organized methods by restructuring musicviewer constructor

--- src/main/ui/gui/MusicViewer.java
+++ src/main/ui/gui/MusicViewer.java
@@ -4,7 +4,7 @@ import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import java.awt.event.WindowEvent;
+//import java.awt.event.WindowEvent;
 
 public class MusicViewer {
     private JFrame frame;
