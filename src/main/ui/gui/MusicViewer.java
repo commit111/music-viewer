2f6d5089795dd7e34d8f7cff3214e62e8b90abd5
linda <llee20@students.cs.ubc.ca>
2023-11-04 11:36:50 -0700

    updated specifications for methods in musicviewer, removed swirl png

--- src/main/ui/gui/MusicViewer.java
+++ src/main/ui/gui/MusicViewer.java
@@ -10,9 +10,10 @@ public class MusicViewer {
     JFrame frame;
 
     public static void main(String[] args) {
-        new MusicViewer();
+        MusicViewer mv = new MusicViewer();
     }
 
+    //Constructor
     public MusicViewer() {
         frame = new JFrame(); //creating instance of JFrame
 
@@ -27,30 +28,11 @@ public class MusicViewer {
 
         b.setBounds(300, 300, 150, 30); //x-axis, y-axis, width, height
 
-        b.addActionListener(new ActionListener() {
-            public void actionPerformed(ActionEvent e) {
-                tf.setText("Making a playlist...");
-                String playlistName = JOptionPane.showInputDialog(frame,"Enter playlist name:");
-                frame.add(makeButton(playlistName));
-            }
-        });
+        setMakePlaylistBtn(b, tf);
 
-        b2.addActionListener(new ActionListener() {
-            public void actionPerformed(ActionEvent e) {
-                tf.setText("Viewing playlists...");
-                JOptionPane.showMessageDialog(frame,"There are no playlists to view yet.");
-            }
-        });
+        setViewPlaylistBtn(b2, tf);
 
-        b3.addActionListener(new ActionListener() {
-            public void actionPerformed(ActionEvent e) {
-                int choice = JOptionPane.showConfirmDialog(frame,"Do you wish to exit the app?");
-                if (choice == 0) {
-                    frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-                    System.exit(0);
-                }
-            }
-        });
+        setExitAppBtn(b3, tf);
 
         frame.add(b); //adding button in JFrame
         frame.add(b2);
@@ -60,6 +42,50 @@ public class MusicViewer {
         setUpJFrame(frame);
     }
 
+    //MODIFIES: frame, tf
+    //EFFECTS:  makes a new 'make playlist' button
+    private void setMakePlaylistBtn(JButton b, JTextField tf) {
+        b.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                tf.setText("Making a playlist...");
+                String playlistName = JOptionPane.showInputDialog(frame,"Enter playlist name:");
+                frame.add(makeButton(playlistName));
+                tf.setText("Welcome to MusicViewer");
+            }
+        });
+    }
+
+    //MODIFIES: frame, tf
+    //EFFECTS:  makes a new 'view playlist' button
+    private void setViewPlaylistBtn(JButton b2, JTextField tf) {
+        b2.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                tf.setText("Viewing playlists...");
+                JOptionPane.showMessageDialog(frame,"There are no playlists to view yet.");
+                tf.setText("Welcome to MusicViewer");
+            }
+        });
+    }
+
+    //MODIFIES: frame, tf
+    //EFFECTS:  makes a new 'exit app' button
+    private void setExitAppBtn(JButton b3, JTextField tf) {
+        b3.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                tf.setText("Exiting the app...");
+                int choice = JOptionPane.showConfirmDialog(frame,"Do you wish to exit the app?");
+                if (choice == 0) {
+                    frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+                    System.exit(0);
+                } else {
+                    tf.setText("Welcome to MusicViewer");
+                }
+            }
+        });
+    }
+
+    //MODIFIES: frame
+    //EFFECTS: sets up the settings for the JFrame
     private void setUpJFrame(JFrame f) {
         f.add(makeBgImage());
         f.setSize(800,800); //400 width and 500 height
@@ -68,6 +94,7 @@ public class MusicViewer {
         f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
     }
 
+    //EFFECTS: makes a label with a background image
     private JLabel makeBgImage() {
         ImageIcon rawImgIcon = new ImageIcon("src/main/ui/gui/abstract.png");
         Image resizedImg = rawImgIcon.getImage().getScaledInstance(900, 900, Image.SCALE_FAST);
@@ -77,6 +104,7 @@ public class MusicViewer {
         return label;
     }
 
+    //EFFECTS: makes a new button with a given label and preset bounds
     private JButton makeButton(String label) {
         JButton btn = new JButton(label); //creating instance of JButton
         btn.setBounds(10,100, 100,50);
