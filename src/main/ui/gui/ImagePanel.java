3ad72f338f112d4aed9b1d236224a57da4b212da
linda <llee20@students.cs.ubc.ca>
2023-11-07 17:08:52 -0800

    made bg and jpanel into a image panel class, added more layout managers

---
+++ src/main/ui/gui/ImagePanel.java
@@ -0,0 +1,22 @@
+package ui.gui;
+
+import javax.swing.*;
+import java.awt.*;
+
+//The following code is referenced to the StackOverflow post:
+//https://stackoverflow.com/questions/30271069/setting-an-image-as-background-for-jframe
+
+public class ImagePanel extends JPanel {
+    private Image bgImage;
+
+    public ImagePanel(Image image, LayoutManager lm) {
+        this.bgImage = image;
+        this.setLayout(lm);
+    }
+
+    @Override
+    protected void paintComponent(Graphics g) {
+        super.paintComponent(g);
+        g.drawImage(bgImage, 0, 0, this);
+    }
+}
--- src/main/ui/gui/MusicViewer.java
+++ src/main/ui/gui/MusicViewer.java
@@ -1,77 +1,128 @@
 package ui.gui;
 
 import javax.swing.*;
+import javax.imageio.ImageIO;
+
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-//import java.awt.event.WindowEvent;
+import java.awt.geom.AffineTransform;
+import java.awt.image.AffineTransformOp;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
 
 public class MusicViewer {
     private JFrame frame;
+    private JPanel menuPanel;
+    private JPanel playlistsPanel;
+    private Image bgImage;
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         MusicViewer mv = new MusicViewer();
     }
 
     //Constructor
-    public MusicViewer() {
+    public MusicViewer() throws IOException {
         frame = new JFrame(); //creating instance of JFrame
+        bgImage = makeBgImage("src/main/ui/gui/abstract.png");
+        playlistsPanel = new ImagePanel(bgImage, new FlowLayout());
+        menuPanel = new ImagePanel(bgImage, new FlowLayout());
 
-        setUpMainMenu(frame);
+        //menuPanel.setBackground(Color.YELLOW);
+        //playlistsPanel.setBackground(Color.GREEN);
+
+        menuPanel.setOpaque(false);
+        playlistsPanel.setOpaque(false);
+
+        frame.add(menuPanel);
+        frame.add(playlistsPanel);
+
+        setUpMainMenu(menuPanel);
         setUpJFrameSettings(frame);
     }
 
-    //MODIFIES: frame
-    //EFFECTS: adds a text field and main menu buttons to the frame
-    private void setUpMainMenu(JFrame frame) {
+    //MODIFIES: panel
+    //EFFECTS: adds a text field and main menu buttons to the panel
+    private void setUpMainMenu(JPanel panel) {
         final JTextField tf = new JTextField();
         tf.setBounds(300,250, 150,25); //x-axis, y-axis, width, height
         tf.setText("Welcome to MusicViewer");
         tf.setEditable(false);
 
         JButton b = new JButton("Make a new playlist"); //creating instance of JButton
-        JButton b2 = new JButton("View playlists");
-        JButton b3 = new JButton("Exit app");
+        JButton b2 = new JButton("Load playlists");
+        JButton b3 = new JButton("Save playlists");
+        JButton b0 = new JButton("Exit app");
 
         setMakePlaylistBtn(b, tf);
-        setViewPlaylistBtn(b2, tf);
-        setExitAppBtn(b3, tf);
+        setLoadPlaylistBtn(b2, tf);
+        setSavePlaylistBtn(b3, tf);
+        setExitAppBtn(b0, tf);
 
-        frame.add(b); //adding button in JFrame
-        frame.add(b2);
-        frame.add(b3);
-        frame.add(tf);
+        panel.add(b); //adding button in JFrame
+        panel.add(b2);
+        panel.add(b3);
+        panel.add(b0);
+        panel.add(tf);
     }
 
-    //MODIFIES: frame, tf
+    //MODIFIES: b, tf
     //EFFECTS:  makes the button produce a 'make playlist' action
     private void setMakePlaylistBtn(JButton b, JTextField tf) {
         b.addActionListener(new ActionListener() {
+            @Override
             public void actionPerformed(ActionEvent e) {
                 tf.setText("Making a playlist...");
                 String playlistName = JOptionPane.showInputDialog(frame,"Enter playlist name:");
-                frame.add(makeButton(playlistName));
+                playlistsPanel.add(makeButton(playlistName));
+                b.revalidate();
                 tf.setText("Welcome to MusicViewer");
             }
         });
     }
 
-    //MODIFIES: frame, tf
+    //MODIFIES: b, tf
     //EFFECTS:  makes the button produce a 'view playlist' action
-    private void setViewPlaylistBtn(JButton b2, JTextField tf) {
-        b2.addActionListener(new ActionListener() {
+    private void setLoadPlaylistBtn(JButton b, JTextField tf) {
+        b.addActionListener(new ActionListener() {
+            @Override
             public void actionPerformed(ActionEvent e) {
-                tf.setText("Viewing playlists...");
-                JOptionPane.showMessageDialog(frame,"There are no playlists to view yet.");
+                tf.setText("Loading playlists...");
+                int choice = JOptionPane.showConfirmDialog(frame,"Do you want to load playlists from file?",
+                        "Select an option", JOptionPane.OK_CANCEL_OPTION);
+                if (choice == 0) {
+                    //load playlists
+                    JOptionPane.showMessageDialog(frame,"There are no playlists to load from file.");
+                }
                 tf.setText("Welcome to MusicViewer");
             }
         });
     }
 
-    //MODIFIES: frame, tf
+    //MODIFIES: b, tf
+    //EFFECTS:  makes the button produce a 'view playlist' action
+    private void setSavePlaylistBtn(JButton b, JTextField tf) {
+        b.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                tf.setText("Saving playlists...");
+                int choice = JOptionPane.showConfirmDialog(frame,"Do you want to save all playlists?",
+                        "Select an option", JOptionPane.OK_CANCEL_OPTION);
+                if (choice == 0) {
+                    //save playlists
+                    JOptionPane.showMessageDialog(frame,"Playlists successfully saved to file!");
+                }
+                tf.setText("Welcome to MusicViewer");
+            }
+        });
+    }
+
+    //MODIFIES: b, tf
     //EFFECTS:  makes the button produce an 'exit app' action
-    private void setExitAppBtn(JButton b3, JTextField tf) {
-        b3.addActionListener(new ActionListener() {
+    private void setExitAppBtn(JButton b, JTextField tf) {
+        b.addActionListener(new ActionListener() {
+            @Override
             public void actionPerformed(ActionEvent e) {
                 tf.setText("Exiting the app...");
                 int choice = JOptionPane.showConfirmDialog(frame,"Do you wish to exit the app?");
@@ -85,30 +136,63 @@ public class MusicViewer {
         });
     }
 
-    //MODIFIES: frame
+    //MODIFIES: f
     //EFFECTS: sets up the settings for the JFrame
-    private void setUpJFrameSettings(JFrame f) {
-        f.add(makeBgImage());
-        f.setSize(800,800); //400 width and 500 height
-        f.setLayout(new FlowLayout()); //using no layout managers
+    private void setUpJFrameSettings(JFrame f) throws IOException {
+        f.setSize(800,800); //800 width and 800 height
+        f.setLayout(new GridLayout(1,2)); //using layout manager
         f.setVisible(true); //making the frame visible
         f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
     }
 
-    //EFFECTS: makes a label with a background image
-    private JLabel makeBgImage() {
-        ImageIcon rawImgIcon = new ImageIcon("src/main/ui/gui/abstract.png");
-        Image resizedImg = rawImgIcon.getImage().getScaledInstance(900, 900, Image.SCALE_FAST);
-        ImageIcon imgIcon = new ImageIcon(resizedImg);
-        JLabel label = new JLabel(imgIcon);
-        label.setBounds(0, 0, 900, 900); // You can use your own values
-        return label;
+    //EFFECTS: returns a properly scaled bg image from a given file path
+    private BufferedImage makeBgImage(String filePath) throws IOException {
+        BufferedImage before = ImageIO.read(new File(filePath));
+        BufferedImage after = scaleImage(before);
+        return after;
+    }
+
+    // The code for scaleBufferedImage() can be referenced to a StackOverflow post:
+    // https://stackoverflow.com/questions/4216123/how-to-scale-a-bufferedimage
+
+    //EFFECTS: scales the buffered image
+    private BufferedImage scaleImage(BufferedImage before) {
+        double sx = 0.9;
+        double sy = 0.9;
+        int beforeWidth = before.getWidth();
+        int beforeHeight = before.getHeight();
+        BufferedImage after = new BufferedImage(beforeWidth, beforeHeight,BufferedImage.TYPE_INT_ARGB);
+        AffineTransform at = new AffineTransform();
+        at.scale(sx,sy);
+        AffineTransformOp scaleOp = new AffineTransformOp(at, AffineTransformOp.TYPE_BILINEAR);
+        after = scaleOp.filter(before, after);
+        return after;
     }
 
     //EFFECTS: makes a new button with a given label and preset bounds
     private JButton makeButton(String label) {
         JButton btn = new JButton(label); //creating instance of JButton
         btn.setBounds(10,100, 100,50);
+        btn.setVisible(true);
         return btn;
     }
 }
+
+
+
+
+//    //--------
+//    //OLD CODE FOR BG IMAGE USING JLabel, TO USE, CALL frame.add(makeBgImage())
+//    //EFFECTS: makes a label with a background image
+//    private JLabel makeBgImage() {
+//        ImageIcon rawImgIcon = new ImageIcon("src/main/ui/gui/abstract.png");
+//        Image resizedImg = rawImgIcon.getImage().getScaledInstance(900, 900, Image.SCALE_FAST);
+//        ImageIcon imgIcon = new ImageIcon(resizedImg);
+//        JLabel label = new JLabel(imgIcon);
+//        label.setBounds(0, 0, 900, 900); // You can use your own values
+//        return label;
+//    }
+//    //--------
+
+
+
